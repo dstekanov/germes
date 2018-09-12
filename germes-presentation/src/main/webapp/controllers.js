7af60c2e5937c3af581214cabfb7d0899459d704
Dmytro_Stekanov <dstekanov@gmail.com>
2018-09-12 14:53:16 +0300

    add controller

---
+++ germes-presentation/src/main/webapp/controllers.js
@@ -0,0 +1,12 @@
+app.controller('CityCtrl', ['$scope', function ($scope) {
+    $scope.cities = [{
+        'name': 'Odessa',
+        'district': '',
+        'region': 'Odessa'
+    },
+        {
+            'name': 'Izmail',
+            'district': 'Izmail',
+            'region': 'Odessa'
+        }];
+}]);
