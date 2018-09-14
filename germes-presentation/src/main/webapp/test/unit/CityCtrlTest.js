bd50d01630b79f934e3504aa9089e0c9c3df6094
Dmytro_Stekanov <dstekanov@gmail.com>
2018-09-14 14:51:02 +0300

    add karma, client side test

---
+++ germes-presentation/src/main/webapp/test/unit/CityCtrlTest.js
@@ -0,0 +1,17 @@
+describe('Testing City Controller', function () {
+    beforeEach(module("app"));
+
+    var scope = {};
+    beforeEach(inject(function ($controller) {
+        $controller('CityCtrl', {$scope: scope});
+    }));
+
+    afterEach(function () {
+        //Cleaning resources/variables
+    });
+
+    it('Rows per page should have initial value', function () {
+        expect(scope.rowsPerPage).toBeDefined();
+        expect(scope.rowsPerPage).toBe(10);
+    });
+});
