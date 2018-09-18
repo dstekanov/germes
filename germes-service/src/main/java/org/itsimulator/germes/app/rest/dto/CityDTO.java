66142eef5076700e855996639326af5115764bd2
Dmytro_Stekanov <dstekanov@gmail.com>
2018-09-18 16:02:05 +0300

    add swagger, add swagger doc

--- germes-service/src/main/java/org/itsimulator/germes/app/rest/dto/CityDTO.java
+++ germes-service/src/main/java/org/itsimulator/germes/app/rest/dto/CityDTO.java
@@ -1,5 +1,7 @@
 package org.itsimulator.germes.app.rest.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import org.itsimulator.germes.app.model.entity.geography.City;
 import org.itsimulator.germes.app.rest.dto.base.BaseDTO;
 
@@ -8,41 +10,45 @@ import org.itsimulator.germes.app.rest.dto.base.BaseDTO;
  *
  * @author Morenets
  */
+@ApiModel(description = "City with transport stations to book and purchase tickets")
 public class CityDTO extends BaseDTO<City> {
-	private String name;
+    private String name;
 
-	/**
-	 * Name of the district where city is placed
-	 */
-	private String district;
+    /**
+     * Name of the district where city is placed
+     */
+    private String district;
 
-	/**
-	 * Name of the region where district is located.
-	 * Region is top-level area in the country
-	 */
-	private String region;
+    /**
+     * Name of the region where district is located.
+     * Region is top-level area in the country
+     */
+    private String region;
 
-	public String getName() {
-		return name;
-	}
+    @ApiModelProperty(name = "Name of the city", required = true)
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getDistrict() {
-		return district;
-	}
+    @ApiModelProperty(name = "Name of the city's district. Empty for region center", required = false)
+    public String getDistrict() {
+        return district;
+    }
 
-	public void setDistrict(String district) {
-		this.district = district;
-	}
+    public void setDistrict(String district) {
+        this.district = district;
+    }
 
-	public String getRegion() {
-		return region;
-	}
+    @ApiModelProperty(name = "Name of the city's region", required = true)
+    public String getRegion() {
+        return region;
+    }
 
-	public void setRegion(String region) {
-		this.region = region;
-	}
+    public void setRegion(String region) {
+        this.region = region;
+    }
 }
